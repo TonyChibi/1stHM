7466328ea3c799a225d19a58dcee0ea7ae43f739
GlazHorsez <to5gogol@gmail.com>
2023-05-21 15:44:15 +0300

    add task n4

---
+++ taskN4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+bool cond=true;
+int max=0;
+do{
+    Console.Write("insert an integer or 'stop' to end the program:  ");
+    string insert = Console.ReadLine()!;
+    if (insert.ToLower()=="stop"){
+        cond=false;
+        Console.WriteLine($"the final maximum is {max}");
+    }
+    else{
+        int num= int.Parse(insert);
+        if (num>max)max=num;
+
+        Console.WriteLine($"the current greatest integer is {max}");
+    }
+}while(cond);
