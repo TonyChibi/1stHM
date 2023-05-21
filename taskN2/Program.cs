6aa7aab794f67a008201a61216fe6431df4a51f7
GlazHorsez <to5gogol@gmail.com>
2023-05-21 15:07:41 +0300

    add task n2

---
+++ taskN2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("insert first integer ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("insert second intger ");
+int b = int.Parse(Console.ReadLine()!);
+if (a>b){
+    Console.Write("the greatest integer is ");
+    Console.WriteLine(a);
+}else Console.Write("the greatest integer is ");
+Console.WriteLine(b);
