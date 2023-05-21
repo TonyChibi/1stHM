788416076215057bfb64a96ccc91de3c64ffc488
GlazHorsez <to5gogol@gmail.com>
2023-05-21 15:55:29 +0300

    add task n6

---
+++ taskN6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("insert integer: ");
+int num = int.Parse(Console.ReadLine()!);
+if(num%2!=0){
+    Console.WriteLine($"the {num} integer is odd");
+}else Console.WriteLine($"the {num} integer is evev");
